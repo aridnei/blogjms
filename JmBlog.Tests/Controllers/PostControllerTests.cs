5edbf298263c0e759292e75ed11334783db8cf62
ernanej <ernanej@jmsegs.com>
2018-11-07 17:23:49 -0200

    Initial commit unity tests

---
+++ JmBlog.Tests/Controllers/PostControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JmBlog.Tests.Controllers
+{
+    public class PostControllerTests
+    {
+        private readonly Mock<IPostService> _mockService;
+        private readonly PostController _controller;
+
+        public PostControllerTests()
+        {
+            _mockService = new Mock<IPostService>();
+            _controller = new PostController(_mockService.Object);
+        }
+
+        [Fact]
+        public void MustCallServiceOnPost()
+        {
+            var request = new Post();
+            _mockService.Setup(x => x.Create(It.IsAny<Post>()));
+
+            _controller.Post(request);
+
+            _mockService.Verify(x => x.Create(It.IsAny<Post>()), Times.Once);
+            _mockService.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void MustThrowExceptionOnPost()
+        {
+            var request = new Post();
+            _mockService.Setup(x => x.Create(It.IsAny<Post>())).Callback(()=> throw new Exception("Error") );
+
+            Assert.Throws<Exception>(_controller.Post(request));
+
+            _mockService.Verify(x => x.Create(It.IsAny<Post>()), Times.Once);
+            _mockService.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ JmBlog.Tests/Services/PostServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using JmBlog.Tests.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JmBlog.Tests.Services
+{
+    public class PostServiceTests
+    {
+        private readonly PostService _service;
+
+        public PostServiceTests()
+        {
+            _service = new PostService();
+        }
+    }
+}
