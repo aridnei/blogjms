9ff66191352d377616e5291e118ac4a802bea879
Igor Janoski dos Santos <igorj@seguradora.jm>
2018-11-08 10:24:42 -0200

    T.U. get api

---
+++ JmBlog.Tests/Data/PostRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using JmBlog.Controllers;
+using JmBlog.Interfaces;
+using JmBlog.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JmBlog.Tests.Controllers
+{
+    public class PostRepositoryTests
+    {
+        private readonly Mock<IPostService> _mockService;
+        private readonly PostController _controller;
+
+        public PostRepositoryTests()
+        {
+            _mockService = new Mock<IPostService>();
+            _controller = new PostController(_mockService.Object);
+        }
+
+        [Fact]
+        public void MustCallServiceOnPost()
+        {
+            var request = new PostCreateViewModel();
+            _mockService.Setup(x => x.Create(It.IsAny<PostCreateViewModel>()));
+
+            _controller.Post(request);
+
+            _mockService.Verify(x => x.Create(It.IsAny<PostCreateViewModel>()), Times.Once);
+            _mockService.VerifyNoOtherCalls();
+        }
+    }
+}
